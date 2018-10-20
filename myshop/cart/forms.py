d55cfb1189f6f13c9445b2699d307645bdeb29b8
babonaresh <nbashaboena@unomaha.edu>
2018-10-19 21:51:21 -0500

    updated code
    updated home page

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
-
-
+#PRODUCT_SIZE_CHOICES = ['XS','S','M','L','XL','XXL','XXXL']
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
                                 choices=PRODUCT_QUANTITY_CHOICES,
                                 coerce=int)
+    #size = forms.CharField(label="SIZE",widget=forms.Select(PRODUCT_QUANTITY_CHOICES))
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
